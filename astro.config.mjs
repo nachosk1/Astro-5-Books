4d8aae43fa80e71f93b28ec98eee23d43f55eb5b
pedro <user@gmail.com>
2025-01-14 14:16:20 -0300

    dependences to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercelServerless from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
+    adapter: vercelServerless({}),
     integrations: [tailwind()],
     env: {
         schema: {
