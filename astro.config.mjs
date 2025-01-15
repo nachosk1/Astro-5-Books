c3fd519fd547d3483838a830d27c64d1b8fe4cc1
Prybet <prybet@proton.me>
2025-01-15 18:39:02 -0300

    Country

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
